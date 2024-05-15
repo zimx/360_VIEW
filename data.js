d1851b212a301c332dab1dbc4d69cb680e43d04c
May Sam <maysssam@gmail.com>
2024-05-14 21:15:33 -0700

    DFSGSDFGSDFG
    SDFGDSFG

---
+++ data.js
@@ -0,0 +1,45 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-demo01",
+      "name": "demo01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.7164825559193924,
+        "pitch": 0.028986454034818365,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0431703232645013,
+          "pitch": -0.22717546118065002,
+          "rotation": 3.141592653589793,
+          "target": "0-demo01"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
